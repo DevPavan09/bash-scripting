ebe783b08724dd5d51ad87719eefabcdff8ea634
Pavan <pavan.bc9@gmail.com>
2023-08-03 19:02:49 +0530

    Added background color code

--- 03-colors.sh
+++ 03-colors.sh
@@ -17,3 +17,9 @@ echo -e "\e[33m I am printing YELLOW Color \e[0m"
 echo -e "\e[32m I am printing Green color \e[0m"
 echo -e "\e[34m I am printing BLUE color \e[0m"
 echo -e "\e[35m I am printing MAgneta color \e[0m"
+
+# To print something with background + foreGround color , here is the syntax: 
+# Ex : 
+#   echo -e "\e[backGrouCode;foreGroudCodem your Message \e[0m"
+
+echo -e "\e[42;36m I am printing background and foreground colors \e[0m"
