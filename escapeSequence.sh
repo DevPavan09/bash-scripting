0d7a7776cf2b22b8387605e87f8d526586566c4f
Pavan <pavan.bc9@gmail.com>
2023-08-03 15:24:26 +0530

    Added \t in the line

---
+++ escapeSequence.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Shebang notation: This tells the script with interpretor this script has to be executed.
+# There can only be one interpretor per script and that should always be the first line of the script.
+
+echo Welcome To Bash Scripting
+
+# This is how we can execute the scripts
+# $ bash scriptName.sh or sh scriptName.sh or ./scriptName.sh
+
+echo Line1
+
+echo Line2
+
+echo Line3
+
+echo Line4
+
+# \n : indicated a new line
+# \t : This gives a tab space
+#   Manoj\nDevOps
+
+echo -e "Line5\nLine6"
+
+# Whenever you're using special characters , always enclose them in DOUBLE QUOTES
+# " "  : Double Quotes 
+# ' '  : Single Quotes
+
+echo -e "Cloud\tDevOps\tare\tBooming"
