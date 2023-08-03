2ccae2da1b6f2041ccfe43de8f2f8bc75afda0b1
Pavan <pavan.bc9@gmail.com>
2023-08-03 23:50:50 +0530

    special variable

---
+++ 06-specialVariables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Special variables gives special properties to your variables 
+
+# Here are few of the special variabes :  $0 to $9 , $? ,  $#,  $* , $@
+
+echo $0
+echo "Executed Script Name is: $0"
+
+# echo "Name of recently launched Rocket in India is
